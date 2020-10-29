f9d4ade0bb540b2c5b252641d055da7fc47a13d1
Edu Rosado <e.rosado.fernandez@gmail.com>
2020-10-29 20:09:56 +0100

    Added basic layout for the chat upper action container

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -7,14 +7,31 @@ export default function Chat() {
 
     const game  = useSelector(state => state.activeGame)
 
+    const handleSkip = () =>{
+        // TODO
+    }
+
     return (
         <div className="m-chat">
             <div className="upper-container">
                 <div className="game-info-actions">
-                    game info and action
+                    <div className="title">{game.title}</div>
+                    <i class="fas fa-address-book"></i>
                 </div>
                 <div className="word-container">
-                    word container
+                    <div className="word-box">
+                        <span>Your word: </span>
+                        <span className="word">XXXXXXX</span>
+                    </div>
+                    <div className="skip-box">
+                        <span>Time to skip: </span>
+                        <span className="time">01:30</span>
+                        <Button
+                            disabled={true}
+                            className="skip-btn"
+                            onClick={handleSkip}
+                        >Skip word</Button>
+                    </div>
                 </div>
             </div>
             <div className="messages-container">
