a15e4af68469659510069d86132abc2ceeed4fa4
Edu Rosado <e.rosado.fernandez@gmail.com>
2020-11-02 17:29:07 +0100

    Fixed run build command for deployment

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use("/api/friends", friendsRouter)
 app.use("/api/contacts", contactsRouter)
 app.use("/api/games", gamesRouter)
 
-if (process.env.NODE__ENV === "production"){
+if (process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"))
     app.get("*", (req,res) => {
         res.sendFile(path.resolve(__dirname, "client","build", "index.html"))
